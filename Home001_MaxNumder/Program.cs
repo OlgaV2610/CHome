267ca12d92d9e0cb00505ab0ccb9814af1cb24a6
Olga <vashkel@yandex.ru>
2022-11-30 15:56:49 +0700

    Diagram

--- Home001_MaxNumder/Program.cs
+++ Home001_MaxNumder/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти максимальное из трех чисел
+Console.WriteLine("Введите 3 целых числа от 1 до 9: ");
+string numberA, numberB = Console.ReadLine()
+
+Console.WriteLine(numberB);
+Console.WriteLine(numberC);
+int numberA;
+int numberB;
+int numberC;
+
+int max = numberA;
+
+if (max < numberB then max = numberB);
+If (max < numberC then max = numberC);
+
