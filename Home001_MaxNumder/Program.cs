7f838a09f367404638d408a5735f641d85005e18
Olga <vashkel@yandex.ru>
2022-11-30 17:37:56 +0700

    Program

--- Home001_MaxNumder/Program.cs
+++ Home001_MaxNumder/Program.cs
@@ -1,15 +1,4 @@
 ﻿//Найти максимальное из трех чисел
 Console.WriteLine("Введите 3 целых числа от 1 до 9: ");
-string numberA, numberB = Console.ReadLine()
-
-Console.WriteLine(numberB);
-Console.WriteLine(numberC);
-int numberA;
-int numberB;
-int numberC;
-
-int max = numberA;
-
-if (max < numberB then max = numberB);
-If (max < numberC then max = numberC);
+string numbers = Console.ReadLine();
 
