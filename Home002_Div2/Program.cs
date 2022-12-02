39d9270b161555ca4afcf4593152b11cb08293c8
Olga <vashkel@yandex.ru>
2022-12-02 12:34:53 +0700

    all

--- Home002_Div2/Program.cs
+++ Home002_Div2/Program.cs
@@ -2,6 +2,13 @@
 //является ли число чётным (делится ли оно на два без остатка).
 //4 -> да, -3 -> нет, 7 -> нет
 
+Console.WriteLine("start"); // начало
+
+int number = 4; // Определили целое число
+int result = number % 2; // вычислениt четных
+Console.WriteLine(result); // Вывод результата
+
+Console.WriteLine("end"); // окончание
 Console.WriteLine("Введите число: ");
 string s = Console.ReadLine();
 int n = Convert.ToInt32(s); 
