d2e3a8c2b077fabd137b06e20d9aa802f106669c
Olga <vashkel@yandex.ru>
2022-12-09 12:05:13 +0700

    задача 23

--- Home021/Program.cs
+++ Home021/Program.cs
@@ -1,16 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
 int ax,ay,bx,by;
 string s;
-Console.Write("Введите пятизначное число: ");
+
+Console.Write("Введите значение координаты x точки a: ");
 s = Console.ReadLine();
 ax = Convert.ToInt32(s);
-Console.Write("Введите пятизначное число: ");
+Console.Write("Введите значение координаты у точки a: ");
 s = Console.ReadLine();
 ay = Convert.ToInt32(s);
-Console.Write("Введите пятизначное число: ");
+Console.Write("Введите значение координаты x точки b: ");
 s = Console.ReadLine();
 bx = Convert.ToInt32(s);
-Console.Write("Введите пятизначное число: ");
+Console.Write("Введите значение координаты y точки b: ");
 s = Console.ReadLine();
 by = Convert.ToInt32(s);
+
 Console.Write(Math.Sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by)));
---
+++ Home023/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число n: ");
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+
+s = s + " -> ";
+for(int i = 1; i <= n; i++){
+    s = s + (i * i * i) + ", ";
+}
+Console.Write(s);
