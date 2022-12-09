666756c96f8cb400bf4905831ae1dfd68707b5e8
Olga <vashkel@yandex.ru>
2022-12-09 11:17:44 +0700

    задача 21

---
+++ Home021/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int ax,ay,bx,by;
+string s;
+Console.Write("Введите пятизначное число: ");
+s = Console.ReadLine();
+ax = Convert.ToInt32(s);
+Console.Write("Введите пятизначное число: ");
+s = Console.ReadLine();
+ay = Convert.ToInt32(s);
+Console.Write("Введите пятизначное число: ");
+s = Console.ReadLine();
+bx = Convert.ToInt32(s);
+Console.Write("Введите пятизначное число: ");
+s = Console.ReadLine();
+by = Convert.ToInt32(s);
+Console.Write(Math.Sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by)));
