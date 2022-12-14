80504e4928a581f1c50b606c2840f744a19d7ba5
Olga <vashkel@yandex.ru>
2022-12-14 13:51:43 +0700

    Home029 done

--- Home029/MyLibrary.cs
+++ Home029/MyLibrary.cs
@@ -1,19 +1,21 @@
 public class MyLibrary
-{
-    public static int[] CreateArray(int count); // создание массива
+{   // создание массива
+    public static int[] CreateArray(int count) 
     {
         return new int[count];
     }
-
-    public static void Fill(int[] array) // заполнение числами от 0 до 14.
-    for (int i = 0; i < array.Length; i++)
+    // заполнение числами от 0 до 14.       
+    public static void Fill(int[] array)
     {
-    array[i] = Random.Shared.Next(0, 15);
-    }
-
+        for (int i = 0; i < array.Length; i++)
+        {
+        array[i] = Random.Shared.Next(0, 15);
+        }
+    } 
+    // печать массива
     public static string Print(int[] array)
     {
-        return String.Join(' ', array); // печать массива
+        return String.Join(' ', array); 
     }
     public static int Pow(int a, int b)
     {
@@ -23,13 +25,12 @@ public class MyLibrary
             result *= a;
         }
         return result; 
-
-
+    }
     public static int ToDec(int[] value)
     {
         int result = 0;
         int count = value.Length;
-        for (int i = 0; i < value.Length; i++)
+        for (int i = 0; i < count; i++)
         {
             result += value[i] * Pow(15, count - 1 - i);
         }
--- Home029/Program.cs
+++ Home029/Program.cs
@@ -4,42 +4,4 @@
 // N: 3 [9, 4, 12] => 2097
 // Для проверки можно использовать https://numsys.ru/convert/2097/10/15
 
-int[] CreateArray(int count); // создание массива
-{
-    return new int[count];
-}
-
-void Fill(int[] array) // заполнение числами от 0 до 14.
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = Random.Shared.Next(0, 15);
-    }
-}
-
-string Print(int[] array)
-{
-    return String.Join(' ', array); // печать массива
-}
-int Pow(int a, int b)
-{
-    int result = 1;
-    for (int i = 0; i < b; i++)
-    {
-        result *= a;
-    }
-    return result; 
-} 
-
-int ToDec(int[] value)
-{
-    int result = 0;
-    int count = value.Length;
-    for (int i = 0; i < value.Length; i++)
-    {
-        result += value[i] * Pow(15, count - 1 - i);
-    }
-    return result;
-
-}
-Console.WriteLine(ToDec(new int[] { 9, 4, 12}));
+System.Console.WriteLine(MyLibrary.ToDec(new int[] {9, 4, 12}));
