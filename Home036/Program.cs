f5b4b34358ea692758b5c0964f3ad1b84c56a38a
Olga <vashkel@yandex.ru>
2022-12-14 21:47:54 +0700

    Home036

--- Home036/Program.cs
+++ Home036/Program.cs
@@ -1,6 +1,48 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+﻿/*  Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+    Найдите сумму элементов, стоящих на нечётных позициях.
+    [3, 7, 23, 12] -> 19
+    [-4, -6, 89, 6] -> 0
+*/
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
 
-Console.WriteLine("Hello, World!");
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+{
+sum = sum + numbers[z];
+}
+    
+Console.WriteLine($"Cумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(-100, 100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] array = new int[4];
+Random rand = new Random();
+for (int x = 0; x < array.length; x++)
+{
+    array[x] = rand.Next(-100, 100);
+    Console.WriteLine("Значение элемента массива " + х + " = " array[x]);
+}
+
+
