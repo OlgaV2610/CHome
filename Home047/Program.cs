d59819bcd80a827361abedc5852c077d23d13428
Olga <vashkel@yandex.ru>
2022-12-22 23:23:32 +0700

    Home047

--- Home047/Program.cs
+++ Home047/Program.cs
@@ -4,3 +4,31 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+int[,] matrix = new int[3,4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    Console.Write("m = 3, n = 4");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
