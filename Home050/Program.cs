e60d0a7fa93f641b432ac5c9872d9cc8af7d3453
Olga <vashkel@yandex.ru>
2022-12-24 17:07:32 +0700

    Home050 done

--- Home050/Program.cs
+++ Home050/Program.cs
@@ -6,3 +6,41 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+FillArray(matrix);
+PrintArray(matrix);
+
+Console.WriteLine("Введите любое целое положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 10)
+{ 
+    Console.Write("-> такого элемента в таблице нет!");
+    
+}
+Console.WriteLine();
+
+
+
--- Home052/Program.cs
+++ Home052/Program.cs
@@ -5,3 +5,28 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите число строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[n, m];
+Random ran = new Random();
+int sum = 0;
+float middle = 0;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        arr[i, j] = ran.Next(0, 100);
+        sum += arr[i, j];
+        middle = sum / (m * n);
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine("middle=" + middle);
+
+Console.ReadLine();
